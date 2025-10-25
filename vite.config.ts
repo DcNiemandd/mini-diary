2e656f39f9a956ee9d82896e1149b78fa5f9d2f0
Miloš Musílek <musilek@proconom.cz>
2025-10-25 02:08:31 +0200

    chore: automatic publishing to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mini-diary/',
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    VitePWA({ 
+    VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg', './assets/*'],
       manifest: {
@@ -18,30 +19,30 @@ export default defineConfig({
         short_name: 'Vite PWA Project',
         theme_color: '#ffffff',
         icons: [
-            {
-                src: 'pwa-64x64.png',
-                sizes: '64x64',
-                type: 'image/png'
-            },
-            {
-                src: 'pwa-192x192.png',
-                sizes: '192x192',
-                type: 'image/png'
-            },
-            {
-                src: 'pwa-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'any'
-            },
-            {
-                src: 'maskable-icon-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'maskable'
-            }
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
         ],
-      }, 
+      },
     })
   ],
 })
