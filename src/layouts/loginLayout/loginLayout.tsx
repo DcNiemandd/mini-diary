fdd71bc664f930122223cecb4dbbd6207be44b42
Miloš Musílek <musilek@proconom.cz>
2025-11-02 20:51:33 +0100

    feat: login layout responsive

--- src/layouts/loginLayout/loginLayout.tsx
+++ src/layouts/loginLayout/loginLayout.tsx
@@ -1,14 +1,8 @@
-import { useContext, useEffect, type FC } from 'react';
+import { type FC } from 'react';
 import { LoginForm } from '../../components/loginForm/loginForm';
-import { AppContext } from '../../contexts/appContext/appContext';
 import style from './loginLayout.module.scss';
 
 export const LoginLayout: FC = () => {
-    const { auth } = useContext(AppContext);
-    useEffect(() => {
-        auth.tryToLogin('tajneHeslo');
-    }, []);
-
     return (
         <div className={style['login-container']}>
             <h1>Minimalistic diary</h1>
