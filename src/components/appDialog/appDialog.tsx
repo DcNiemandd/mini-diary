10211615ccaf7bf5bc47a8f8adb8e64cb3f43628
Miloš Musílek <milos.musilek@email.cz>
2026-05-04 00:11:16 +0200

    feat: upgraded password change

--- src/components/appDialog/appDialog.tsx
+++ src/components/appDialog/appDialog.tsx
@@ -1,17 +1,23 @@
-import type { DialogOptions, OpenDialogFn } from '../../../lib/dialog/index.ts';
+import type { DialogButton, DialogButtonTypes, DialogOptions, DialogResult } from '../../../lib/dialog/index.ts';
 import { openDialog } from '../../../lib/dialog/index.ts';
 import style from './appDialog.module.scss';
 
-export const openAppDialog: OpenDialogFn = ((options: DialogOptions) => {
+type AppDialogButtonTypes<O> = O extends { buttons: readonly DialogButton[] } ? DialogButtonTypes<O> : 'cancel';
+
+export const openAppDialog = <const O extends DialogOptions>(
+    options: O,
+): Promise<DialogResult<AppDialogButtonTypes<O>>> => {
     return openDialog({
         ...options,
         className: `${style.dialog} ${options.className ?? ''}`,
-        buttons: options.buttons?.map((btn) => ({
-            ...btn,
-            className: `${style.button} ${btn.className ?? ''}`,
-        })),
+        buttons: options.buttons
+            ? options.buttons.map((btn) => ({
+                  ...btn,
+                  className: `${style.button} ${btn.className ?? ''}`,
+              }))
+            : [{ label: 'Ok', type: 'cancel' as const }],
         title: options.title && <h2>{options.title}</h2>,
         content: options.content && <div className={style.content}>{options.content}</div>,
-    });
-}) as OpenDialogFn;
+    }) as Promise<DialogResult<AppDialogButtonTypes<O>>>;
+};
 
--- src/components/changePasswordDialog/changePasswordDialog.tsx
+++ src/components/changePasswordDialog/changePasswordDialog.tsx
@@ -1,40 +1,58 @@
-import { useState, type FC } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState, type FC } from 'react';
 import { useDialog } from '../../../lib/dialog/index.ts';
 import type { AuthState } from '../../hooks/useAuth.ts';
 import { openAppDialog } from '../appDialog/appDialog.tsx';
 import styles from './changePasswordDialog.module.css';
 
 export const ChangePasswordDialog: FC<{ changePassword: AuthState['changePassword'] }> = ({ changePassword }) => {
+    const formRef = useRef<HTMLFormElement>(null);
     const { onButtonClick, disableButtons } = useDialog<'confirm' | 'cancel'>();
     const [oldPassword, setOldPassword] = useState('');
     const [newPassword1, setNewPassword1] = useState('');
     const [newPassword2, setNewPassword2] = useState('');
 
-    onButtonClick('confirm', async () => {
-        if (newPassword1 !== newPassword2) {
-            await openAppDialog({ title: 'Error', content: "New passwords don't match" });
-            return true;
-        }
-        if (oldPassword.length < 6 || newPassword1.length < 6) {
-            await openAppDialog({ title: 'Error', content: 'Password has to be at least 6 characters' });
-            return true;
-        }
-        disableButtons(true);
-        const ok = await changePassword(oldPassword, newPassword1);
-        disableButtons(false);
-        if (!ok) {
-            await openAppDialog({ title: 'Error', content: 'There was problem. Check your old password.' });
-            return true;
-        }
-        return false;
+    const registerDialog = useLayoutEffect(() => {
+        onButtonClick('confirm', async () => {
+            formRef.current?.requestSubmit();
+            if (!formRef.current?.checkValidity()) {
+                return true;
+            }
+
+            if (newPassword1 !== newPassword2) {
+                await openAppDialog({
+                    title: 'Error',
+                    content: "New passwords don't match",
+                });
+                return true;
+            }
+            disableButtons(true);
+            const ok = await changePassword(oldPassword, newPassword1);
+            disableButtons(false);
+            if (!ok) {
+                await openAppDialog({
+                    title: 'Error',
+                    content: 'There was problem. Check your old password.',
+                });
+            }
+
+            await openAppDialog({ title: 'Password changed successfully' });
+
+            return false;
+        });
     });
+    useEffect(() => registerDialog, [registerDialog]);
 
     return (
-        <div className={styles['content']}>
+        <form
+            ref={formRef}
+            className={styles['content']}
+            onSubmit={(e) => e.preventDefault()}
+        >
             <label className={styles['two-columns']}>
                 <span>Old password</span>
                 <input
                     type="password"
+                    minLength={6}
                     value={oldPassword}
                     onChange={(e) => setOldPassword(e.currentTarget.value)}
                 />
@@ -43,6 +61,7 @@ export const ChangePasswordDialog: FC<{ changePassword: AuthState['changePasswor
                 <span>New password</span>
                 <input
                     type="password"
+                    minLength={6}
                     value={newPassword1}
                     onChange={(e) => setNewPassword1(e.currentTarget.value)}
                 />
@@ -51,11 +70,12 @@ export const ChangePasswordDialog: FC<{ changePassword: AuthState['changePasswor
                 <span>New password</span>
                 <input
                     type="password"
+                    minLength={6}
                     value={newPassword2}
                     onChange={(e) => setNewPassword2(e.currentTarget.value)}
                 />
             </label>
-        </div>
+        </form>
     );
 };
 
